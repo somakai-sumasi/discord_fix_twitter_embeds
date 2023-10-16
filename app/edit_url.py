9f5eb6b1b8b99d2a4054cf90c1b844e040fb623a
somakai_sumasi <ueki@novasto.co.jp>
2023-10-16 14:34:13 +0900

    first commit

---
+++ app/edit_url.py
@@ -0,0 +1,17 @@
+import re
+from urllib.parse import urlparse
+
+def edit_twitter_url(txt: str):
+    result = None
+    for match in re.finditer(r"https?://[\w/:%#\$&\?\(\)~\.=\+\-]+", txt):
+        url = match.group()
+        parse = urlparse(url)
+
+        domain = parse.netloc 
+        if(domain != 'twitter.com' and domain != 'x.com'):
+            continue
+
+        convert_parse = parse._replace(netloc="vxtwitter.com")
+        result = convert_parse.geturl() + "\n"
+
+    return result
---
+++ app/main.py
@@ -0,0 +1,37 @@
+import math
+import os
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+from edit_url import edit_twitter_url
+
+load_dotenv()
+intents = discord.Intents.all()
+activity = discord.Activity(name="MyBot", type=discord.ActivityType.custom)
+bot = commands.Bot(
+    command_prefix="!", intents=intents, activity=activity, help_command=None
+)
+
+
+# bot起動時
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+
+
+# メッセージ受信時のイベント
+@bot.event
+async def on_message(message: discord.Message):
+    if len(message.embeds) != 0:
+     return
+    
+    url = edit_twitter_url(message.content)
+    if(url == None):
+        return None
+
+    await message.channel.send(url)
+
+
+TOKEN = os.getenv("TOKEN")
+bot.run(TOKEN)
